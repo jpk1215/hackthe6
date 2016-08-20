0b7742ef7d9ad1291a8db176e024ffd8a9ee6d9a
Jeffrey Klaus <jpk1215@gmail.com>
2016-08-20 16:40:23 -0400

    have nodemailer working

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var app = express();
 var pizzapi = require('pizzapi');
+var nodemailer = require('nodemailer');
 
 app.use(express.static('public'));
 
@@ -10,10 +11,29 @@ app.get('/', function(req, res) {
 	res.sendFile(view_path);
 });
 
-app.post('/order', function(req, res) {	
-//code to place order...	
-// 860 richmond street west, Toronto, ON M6J 1C9
-//send an email
+app.post('/order', function(req, res) {
+	var transporter = nodemailer.createTransport('smtps://EXTREMEDEWD%40gmail.com:dothedew@smtp.gmail.com');
+
+	// setup e-mail data with unicode symbols
+	var mailOptions = {
+		from: '"MR DEW" <EXTREMEDEWD@gmail.com>', // sender address
+		to: 'jpk1215@gmail.com', // list of receivers
+		subject: "DUDE YOU'RE GETTING A DEW", // Subject line
+		text: "AN ICE COLD DEW IS BEING BREWED FOR YOU RIGHT NOW. SIT BACK, RELAX, AND AWAIT THAT ICEY COLD RIVER OF LIQUID GOLD THAT IS THE DEW. CONGRATULATIONS, YOU\'VE EARNED IT." , // plaintext body
+		html: '<b>AN ICE COLD DEW IS BEING BREWED FOR YOU RIGHT NOW. SIT BACK, RELAX, AND AWAIT THAT ICEY COLD RIVER OF LIQUID GOLD THAT IS THE DEW. CONGRATULATIONS, YOU\'VE EARNED IT.</b>' // html body
+	};
+
+	// send mail with defined transport object
+	transporter.sendMail(mailOptions, function(error, info){
+		if(error){
+			return console.log(error);
+		}
+		console.log('Message sent: ' + info.response);
+		res.send(200)
+	});
+
 });
 
 app.listen(3000, function() {});
+
+
--- public/client.js
+++ public/client.js
@@ -1,29 +1,38 @@
-console.log('it works');
-console.log($);
-
 $(document).ready(
     function(){
+        var initialized = false;
+        var list = $('ol')[0];
         $('#button').click(
             function(){
-                var toAdd = $('input[name=ListItem]').val();
-                $('ol').append('<li>' + toAdd + '</li>');
+                var input = $('input[name=ListItem]');
+                if(input.val().length > 0) {
+                    initialized = true;
+                    var toAdd = input.val();
+                    $('ol').append('<li>' + toAdd + '</li>');
+                    input.val("");
+                }
             });
 
         $("input[name=ListItem]").keyup(function(event){
-            if(event.keyCode == 13){
+            if(event.keyCode === 13){
                 $("#button").click();
             }
         });
 
         $(document).on('dblclick','li', function(){
-            $(this).toggleClass('strike').fadeOut('slow');
+            $(this).toggleClass('strike').fadeOut('slow').remove();
+            if(list.childElementCount === 0) {
+                initialized = false;
+                jQuery.post( '/order', {dew: true}, function(data){
+                    console.log('destiny', data)
+                })
+            }
         });
 
         $('input').focus(function() {
             $(this).val('');
         });
 
-        $('ol').sortable();
 
     }
 );
